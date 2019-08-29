26a8d48adc9636583f522df42b80a36630852e77
Lucas Costa <lucashenriqueev@gmail.com>
2019-08-29 10:25:01 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaPOO01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProvaPOO01
+{
+    class Escola
+    {
+        private string anoLetivo;
+        private decimal anuidade;
+
+        public Escola(string anoLetivo, decimal anuidade)
+        {
+            this.anoLetivo = anoLetivo;
+            this.anuidade = anuidade;
+        }
+        public void SetAnoLetivo(string anoLetivo){
+            this.anoLetivo = anoLetivo;
+        }
+        public void SetAnuidade(decimal anuidade) {
+            this.anuidade = anuidade;
+        }
+        public decimal GetValorMensal()
+        {
+            return anuidade / 12;
+        }
+        public decimal GetValorComDesconto()
+        {
+            return anuidade * 90 / 100;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Escola x = new Escola("1º ano", 12000);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
